918faf07533dade420e8b3fe65921e09e1b535e9
Arash <arashyadegari0402@gmail.com>
2022-11-10 08:30:25 +0330

    remove img

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import {BrowserRouter as Router} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <Router>
-          <App />
-      </Router>
+    <React.StrictMode>
+        <Router>
+            <App/>
+        </Router>
 
-  </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Page from "../components/Page";
 import SideBar from "../components/SideBar";
-import Graph from "../const/images/img_1.png";
 import {Container} from "@mui/material";
 
 function Home(props) {
@@ -9,11 +8,6 @@ function Home(props) {
         <Page>
             <SideBar/>
             <Container>
-                <img
-                    height={'80%'}
-                    width={'80%'}
-                    src={Graph}
-                    alt={'graph loop'}/>
             </Container>
         </Page>
     );
